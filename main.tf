a3d2f7eb251148d00c4e515e63d69f32c5182983
the-llama <30959012+the-llama@users.noreply.github.com>
2017-08-12 14:12:54 -0600

    Update main.tf
    Create main VPC and secondary VPC

--- main.tf
+++ main.tf
@@ -4,3 +4,22 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
 }
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "dedicated"
+
+  tags {
+    Name = "main"
+  }
+}
+
+resource "aws_vpc" "secondary" {
+  cidr_block       = "172.16.0.0/16"
+  instance_tenancy = "dedicated"
+
+  tags {
+    Name = "secondary"
+  }
+}
+
